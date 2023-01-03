10fc75beacd9921f5bdf061bcbedb20620230037
Isaac Wanger <teryima127@gmail.com>
2023-01-03 12:59:24 +0100

    user information

--- models/models.go
+++ models/models.go
@@ -55,8 +55,8 @@ type Order struct {
 	Order_Cart				[]ProductUser			`json:"order_list" bson:"order_list"`
 	Ordered_At				time.Time				`json:"ordered_at" bson:"ordered_at"`
 	Price					int						`json:"total_price" bson:"total_price"`
-	Discount				*int					`json:"payment_method" bson:"payment_method"`
-	Payment_Method			Payment
+	Discount				*int					`json:"discount" bson:"discount"`					
+	Payment_Method			Payment					`json:"payment_method" bson:"payment_method"`
 
 }
 
