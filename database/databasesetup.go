02750d5a0ea7e1907a84fd28a4bee79ec687a366
Isaac Wanger <teryima127@gmail.com>
2023-01-03 17:28:22 +0100

    database connection

---
+++ database/databasesetup.go
@@ -0,0 +1,52 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// DBset connects to mongodb
+func DBSet() *mongo.Client {
+	// Create a client
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27107"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Context for DB
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	defer cancel()
+
+	// Connect client to DB
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Test connection
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal("failed to connect to mongodb")
+		return nil
+	}
+
+	fmt. Println("successfully connected to mongodb")
+	return client
+}
+
+var client *mongo.Client = DBSet()
+
+func Userdata(client *mongo.Client, collectionName string) *mongo.Collection {
+
+}
+
+func productData(client *mongo.Client, collectionName string) *mongo.Collection {
+
+}
