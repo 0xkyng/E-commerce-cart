9ecc7e8d0ecb00ef2bc5d43294abe74bc61f4755
Isaac Wanger <teryima127@gmail.com>
2023-01-10 10:24:55 +0100

    delete address

--- contollers/address.go
+++ contollers/address.go
@@ -1,6 +1,15 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/codekyng/E-commerce-cart.git/models"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 func AddAddress() gin.HandlerFunc {
 
@@ -14,6 +23,45 @@ func EditWorkAddress() gin.HandlerFunc {
 
 }
 
+// DeleteAddress sets the address to be 
+// Deleted to an empty value
 func DeleteAddress() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Find the user id whose
+		// You want to delete
+		user_id := c.Query("id")
+
+		// Check if user id is empty.
+		if user_id == ""{
+			c.Header("Content-Type", "application/json")
+			c.JSON(http.StatusNotFound, gin.H{"Error":"Invalid Search Index"})
+			c.Abort()
+			return
+		}
+
+		// Create an empty slice of type address
+		addresses := make([]models.Address, 0)
+		usert_id, err := primitive.ObjectIDFromHex(user_id)
+		if err != nil {
+			c.IndentedJSON(500, "Internal Server Error")
+		}
+
+		// Set context
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+		filter := bson.D{primitive.E{Key:"_id", Value: usert_id}}
+		// Set the user address to empty value
+		update := bson.D{primitive.E{Key: "$set", Value: bson.D{primitive.E{Key: "address", Value: addresses}}}}
+		_, err = UserCollection.UpdateOne(ctx, filter, update)
+		if err != nil {
+			c.IndentedJSON(404, "Wrong command")
+			return
+		}
+		defer cancel()
+		ctx.Done()
+		c.IndentedJSON(200, "Succesfylly Deleted")
+
+
+	}
 
 }
