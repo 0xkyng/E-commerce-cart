9afe6d62ca6c4d50fe44cbaf68e2ad1b47940e9b
Isaac Wanger <teryima127@gmail.com>
2023-01-11 11:22:07 +0100

    token

---
+++ middleware/middleware.go
@@ -0,0 +1,35 @@
+package middleware
+
+import (
+	"net/http"
+
+	token "github.com/codekyng/E-commerce-cart.git/tokens"
+	"github.com/gin-gonic/gin"
+)
+
+func Authentication() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Get token
+		ClientToken := c.Request.Header.Get("token")
+		// Check if token is empty
+		if ClientToken == ""{
+			c.JSON(http.StatusInternalServerError, gin.H{"error":"No authorization header provided"})
+			c.Abort()
+			return
+		}
+
+		// Validate token
+		claims, err := token.ValidateToken(ClientToken)
+		if err != "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error":err})
+			c.Abort()
+			return
+		}
+
+		c.Set("email", claims.Email)
+		c.Set("uid", claims.Uid)
+		c.Next()
+
+	}
+
+}
