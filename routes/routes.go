489035c4a3b855bebb43f878f310574228f86f86
Isaac Wanger <teryima127@gmail.com>
2023-01-13 19:43:05 +0100

    Routes file

--- routes/routes.go
+++ routes/routes.go
@@ -5,6 +5,7 @@ import (
 	"github.com/codekyng/E-commerce-cart.git/controllers"
 )
 
+// UserRoutes
 func UserRoutes(incomingRoutes *gin.Engine) {
 	incomingRoutes.POST("/users/signup", controllers.SignUp())
 	incomingRoutes.POST("/users/login", controllers.Login())
